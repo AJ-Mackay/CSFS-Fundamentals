02b9c26fb5c1969afcbbaf9138efeeb545ab1ff1
Ruiner85 <paul.m.mackay85@gmail.com>
2020-08-13 14:27:52 +0100

    add booleanValues

---
+++ booleanValues/booleanValues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace booleanValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool myFirstBoolean = 3 + 2 == 5;
+            Console.WriteLine(myFirstBoolean.ToString());
+            bool mySecondBoolean = 3 + 2 > 5;
+            Console.WriteLine(mySecondBoolean.ToString());
+            bool bothAreTrue = myFirstBoolean && mySecondBoolean;
+            Console.WriteLine("bothAreTrue evaluates " + bothAreTrue);
+            bool oneIsTrue = myFirstBoolean || mySecondBoolean;
+            Console.WriteLine("oneIsTrue evaluates {0} because one expression is true", oneIsTrue);
+            bool reverseMyFirstBoolean = !myFirstBoolean;
+            Console.WriteLine("myFirstBoolean is {0} but reverseMyFirstBoolean is {1}", myFirstBoolean,
+                reverseMyFirstBoolean);
+        }
+    }
+}
