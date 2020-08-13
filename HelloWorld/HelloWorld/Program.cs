9354edded5d1d8167c8c95cbe8aadb71637c7c64
Ruiner85 <paul.m.mackay85@gmail.com>
2020-08-13 13:26:59 +0100

    initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < UPPER; i++)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
